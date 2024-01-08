97f565fc59248353e08900504f6cd3f07494147b
futoke <futoke@gmail.com>
2024-01-08 23:38:55 +0300

    upd

--- start-browser.sh
+++ start-browser.sh
@@ -2,4 +2,8 @@
 
 export DISPALY=:0
 # chromium-browser --noerrdialogs --disable-infobars --disable-translate --no-first-run --fast --fast-start --aggressive-cache-discard --kiosk $1 &
-chromium-browser --kiosk $1 &
+if command -v chromium-browser > /dev/null; then
+    chromium-browser --kiosk $1 &
+else
+    chromium --kiosk $1 &
+fi
